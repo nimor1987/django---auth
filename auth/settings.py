ba113f3e1989a0f67bb3634fa0ee3d923b7b6002
nimor1987 <75226034+nimor1987@users.noreply.github.com>
2020-12-11 03:30:52 +0100

    Update settings.py

--- auth/settings.py
+++ auth/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'o3n^s*e8js1t6$u9ana4aurif6+p!iyzy$#z6rb0)$%&zm-v(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
